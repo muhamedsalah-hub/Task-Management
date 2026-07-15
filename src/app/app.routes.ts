70457f479eaa15c19e7c82c7879ab0fac305cf67
Mohamed Salah <Mohamedsalah1999@gmail.com>
2026-07-15 19:36:39 +0300

    Completed Auth layout and Auth routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
+import { LoginComponent } from './components/Auth/login/login.component';
+import { SignUpComponent } from './components/Auth/sign-up/sign-up.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: AuthLayoutComponent,
+    children: [
+      { path: '', redirectTo: 'login', pathMatch: 'full' },
+      { path: 'login', component: LoginComponent },
+      { path: 'signup', component: SignUpComponent },
+    ],
+  },
+];
