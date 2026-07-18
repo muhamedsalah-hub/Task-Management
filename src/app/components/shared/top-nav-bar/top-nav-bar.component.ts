8653377a2011a0823015f9a71fb13bf4ea59477a
Mohamed Salah <Mohamedsalah1999@gmail.com>
2026-07-18 19:00:16 +0300

    handled top nav bar UI

--- src/app/components/shared/top-nav-bar/top-nav-bar.component.ts
+++ src/app/components/shared/top-nav-bar/top-nav-bar.component.ts
@@ -1,25 +1,20 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, PLATFORM_ID } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 import { AuthService } from '../../../core/services/auth.service';
 import { IUserdata } from '../../../core/interfaces/Auth/types';
 import { TrimTextPipe } from '../../../core/pipes/trim-text.pipe';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-top-nav-bar',
   standalone: true,
-  imports: [FontAwesomeModule,TrimTextPipe],
+  imports: [FontAwesomeModule, TrimTextPipe],
   templateUrl: './top-nav-bar.component.html',
   styleUrl: './top-nav-bar.component.css',
 })
 export class TopNavBarComponent {
   readonly faBars = faBars;
-  user: IUserdata | null = null;
+
   private readonly _AuthService = inject(AuthService);
-  
-  constructor() {
-    this.user = this._AuthService.user;
-    console.log(this.user);
-    
-  }
 }
--- src/app/core/pipes/trim-text.pipe.ts
+++ src/app/core/pipes/trim-text.pipe.ts
@@ -5,9 +5,10 @@ import { Pipe, PipeTransform } from '@angular/core';
   standalone: true,
 })
 export class TrimTextPipe implements PipeTransform {
-  transform(value: string, ...args: unknown[]): string {
+  transform(value: string, ...args: unknown[]): any {
+    if(!value) return ;
     const firstName = value.split(' ')[0] ;
     const lastName = value.split(' ')[1];
-    return lastName.padStart(2, firstName[0]);
+    return lastName[0].padStart(2, firstName[0]);
   }
 }
